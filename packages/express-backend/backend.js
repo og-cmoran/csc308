025e587b4f957d9333b94bffde51cb3e7e5bd6ae
og-cmoran <cmoran06@calpoly.edu>
2025-10-07 23:45:08 -0700

    added getting users by id

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -37,6 +37,8 @@ const findUserByName = (name) => {
       (user) => user["name"] === name
     );
 };
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
 
 app.use(express.json());
   
@@ -53,6 +55,16 @@ app.get("/users", (req, res) => {
     } else {
       res.send(users);
     }
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
 });
 
 app.listen(port, () => {
