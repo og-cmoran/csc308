30d58879a547e9a3353a7f4f5387609034a47216
og-cmoran <cmoran06@calpoly.edu>
2025-12-03 10:41:52 -0800

    tests added

--- projects/sample-functions.test.js
+++ projects/sample-functions.test.js
@@ -7,6 +7,36 @@ test('Testing div -- success', () => {
   expect(result).toBe(target);
 });
 
+test('Testing div -- pos num divided by a neg is neg', () => {
+  const target = -2;
+  const result = myFunctions.div(10, -5);;
+  expect(result).toBe(target);
+});
+
+test('Testing div -- neg num divided by a neg is pos', () => {
+  const target = 2;
+  const result = myFunctions.div(-10, -5);
+  expect(result).toBe(target);
+});
+
+test('Testing div -- zero divided by a num is zero', () => {
+  const target = 0;
+  const result = myFunctions.div(0, 1);
+  expect(result).toBe(target);
+});
+
+test('Testing div -- division by one is the same number', () => {
+  const target = 2;
+  const result = myFunctions.div(2, 1);
+  expect(result).toBe(target);
+});
+
+test('Testing div -- division by zero is infinite', () => {
+  const target = Infinity;
+  const result = myFunctions.div(20, 0);
+  expect(result).toBe(target);
+});
+
 test('Testing contains numbers -- success', () => {
     const target = true;
     const result = myFunctions.containsNumbers("aaa123q");
